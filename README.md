64044fa59864bba79585eabb03725d5521614e00
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2017-12-20 14:13:23 +0100

    Revert "updates for layer"
    This reverts commit 2b826e596596b6de6242583d6bf8bfe07b795f57.

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # SSL Termination Proxy
 
-This charm installs an [HTTPS reverse proxy](https://en.wikipedia.org/wiki/TLS_termination_proxy).
-The proxy functions as an entrypoint for an entire model. By the use of [subordinate charms](https://github.com/tengu-team/layer-ssl-termination-client), multiple webservices can be connected, each with their own set of domain names and basic_auth data. It also provides loadbalancing per webservice, configured for each webservice, this by the subordinate charms.
-
-The proxy secures traffic to a webservice in the private network using a Let's Encrypt HTTPS certificate.
+This charm installs an [HTTPS reverse proxy](https://en.wikipedia.org/wiki/TLS_termination_proxy). The proxy secures traffic to a webservice in the private network using a Let's Encrypt HTTPS certificate. The proxy can also add basic username/password authentication if the `credentials` config option is set.
 
 <img src="https://raw.githubusercontent.com/tengu-team/layer-ssl-termination-proxy/master/docs/ssl-termination-proxy.png">
 
@@ -16,8 +13,7 @@ This proxy receives an A+ rating on the [Qualis SSL Server Test](https://www.ssl
 ```bash
 # Deploy your http webservice.
 juju deploy jenkins
-# Deploy your proxy-client
-juju deploy cs:~tengu-team/ssl-termination-client ssl-jenkins
+
 # Deploy the Proxy.
 juju deploy cs:~tengu-team/ssl-termination-proxy
 # Expose the proxy.
@@ -25,25 +21,34 @@ juju expose ssl-termination-proxy
 # Configure your DNS server to point to the ssl-termination-proxy's public ip.
 # Let the proxy know what its DNS name is.
 # (See https://www.duckdns.org for free DNS names)
-# Configure your ssl-client with the webservice-specific configs
-# For more detail, see the https://github.com/tengu-team/layer-ssl-termination-client
-juju config ssl-jenkins fqdns="example.com www.example.com"
-# Connect the webservice with the ssl client
-juju add-relation jenkins ssl-jenkins
-# Connect the ssl-client with the proxy.
-juju add-relation ssl-jenkins:ssltermination ssl-termination-proxy:ssltermination
-# Now you can surf to https://example.com and you wil reach the webservice.
+juju config ssl-termination-proxy fqdn=www.example.com
+# The proxy will now request a certificate from lets encrypt.
+
+# Connect the webservice with the proxy.
+juju add-relation jenkins ssl-termination-proxy
+
+# Now you can surf to https://<proxy-public-ip> and you wil reach the webservice.
 ```
 
 ***OpenStack environments***
 If you're using an OpenStack private cloud which uses floating IP addresses, you'll need to associate a floating IP address with the ssl-termination-proxy unit before setting the FQDN, and ensure that this FQDN is reachable from the public Internet.  This is necessary for the Let's Encrypt registration to complete.
 
+**[Optional] Configure basic auth**
+
+```bash
+juju config ssl-termination-proxy credentials="<username> <password>"
+```
+
+*Multiple accounts aren't supported for the moment.*
+
 *Note: Authentication is turned off for `OPTIONS` requests because this is required for CORS. As part of CORS preflight, `OPTIONS` will get called without authentication headers. If this call fails (with 401 unauthorized), the actual CORS call will not be initiated.*
+
+
 
 ## Authors
 
-This software was created in the [IBCN research group](https://www.ibcn.intec.ugent.be/) of [Ghent University](https://www.ugent.be/en) in Belgium. This software is used in [Tengu](https://tengu.io), a project that aims to make experimenting with data frameworks and tools as easy as possible.
+This software was created in the [IBCN research group](https://www.ibcn.intec.ugent.be/) of [Ghent University](https://www.ugent.be/en) in Belgium. This software is used in [Tengu](https://tengu.intec.ugent.be), a project that aims to make experimenting with data frameworks and tools as easy as possible.
 
  - Sander Borny <sander.borny@ugent.be>
  - Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
- - Mathijs Moerman <mathijs.moerman@tengu.io>
+ - Mathijs Moerman <mathijs.moerman@qrama.io>
--- reactive/ssl_termination_proxy.py
+++ reactive/ssl_termination_proxy.py
@@ -14,110 +14,105 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
-from subprocess import check_call, CalledProcessError
+from subprocess import check_call
 
-from charmhelpers.core import hookenv, unitdata
+from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
 
 from charms.reactive import when, when_not, set_state, remove_state
 
 from charms.layer import lets_encrypt  # pylint:disable=E0611,E0401
-from charms.layer.nginx import configure_site
+from charms.layer.nginx import configure_site  # pylint:disable=E0611,E0401
 
 
-db = unitdata.kv()
 config = hookenv.config()
 
 
-@when('apt.installed.apache2-utils')
-@when_not('ssl-termination-proxy.installed')
+@when(
+    'apt.installed.apache2-utils')
+@when_not(
+    'ssl-termination-proxy.installed')
 def install():
-    if not os.path.isdir('/etc/nginx/.htpasswd'):
-        os.mkdir('/etc/nginx/.htpasswd')
     set_state('ssl-termination-proxy.installed')
 
 
-@when('ssl-termination-proxy.installed')
-@when_not('ssltermination.available')
+@when(
+    'ssl-termination-proxy.installed')
+@when_not(
+    'lets-encrypt.registered')
+def signal_need_fqdn():
+    # This check is here to make sure we don't overwrite the "register cert failed" message
+    if not config['fqdn']:
+        status_set('blocked', 'Please fill in fqdn for ssl certificate.')
+
+
+@when(
+    'ssl-termination-proxy.installed',
+    'lets-encrypt.registered')
+@when_not(
+    'reverseproxy.available')
 def signal_need_webservice():
-    status_set('blocked', 'Please relate a SSL Termination client')
+    status_set(
+        'blocked',
+        'Please relate an HTTP webservice (registered {})'.format(config['fqdn']))
 
 
-@when('ssltermination.connected')
-def check_status(ssltermination):
-    ssltermination.check_status()
+@when(
+    'ssl-termination-proxy.running',
+    'config.changed.credentials')
+def configure_basic_auth():
+    print('Credentials changed, re-triggering setup.')
+    remove_state('ssl-termination-proxy.running')
+    # To make sure we don't trigger an infinite loop.
+    remove_state('config.changed.credentials')
 
 
-@when('ssl-termination-proxy.installed', 'ssltermination.available')
-def pre_setup(ssltermination):
-    status_set('maintenance', 'SSL termination relation found, configuring proxy')
-    received_fqdns = []
-    received_basic_auth = []
-    received_private_ips = []
-    received_loadbalancing = []
-    fqdns = db.get('fqdns', [])
-    basic_auth = db.get('basic_auth', [])
-    private_ips = db.get('private_ips', [])
-    loadbalancing = db.get('loadbalancing', [])
-    for data in ssltermination.get_data():
-        received_fqdns.extend(data['fqdns'])
-        received_basic_auth.extend(data['basic_auth'])
-        received_private_ips.extend(data['private_ips'])
-        received_loadbalancing.append(data['loadbalancing'])
-    if fqdns != received_fqdns:
-        db.set('fqdns', received_fqdns)
-        lets_encrypt.update_fqdns()
-        remove_state('ssl-termination-proxy.running')
-    else:
-        status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
-    if basic_auth != received_basic_auth:
-        db.set('basic_auth', received_basic_auth)
-        remove_state('ssl-termination-proxy.running')
-    else:
-        status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
-    if private_ips != received_private_ips:
-        db.set('private_ips', received_private_ips)
-        remove_state('ssl-termination-proxy.running')
-    else:
-        status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
-    if loadbalancing != received_loadbalancing:
-        db.set('loadbalancing', received_loadbalancing)
-        remove_state('ssl-termination-proxy.running')
-    else:
-        status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
-
-
-@when('ssl-termination-proxy.installed', 'ssltermination.available', 'lets-encrypt.registered')
-@when_not('ssl-termination-proxy.running')
-def setup(ssltermination):
-    live = lets_encrypt.live(db.get('fqdns'))
-    for data in ssltermination.get_data():
-        service = data['service']
-        try:
-            os.remove('/etc/nginx/.htpasswd/{}'.format(service))
-        except OSError:
-            pass
-        # Did we get credentials? If so, configure them.
-        for user in data['basic_auth']:
-            try:
-                check_call([
-                    'htpasswd', '-b', '/etc/nginx/.htpasswd/{}'.format(service),
-                    user['name'], user['password']])
-            except CalledProcessError:
-                check_call([
-                    'htpasswd', '-bc', '/etc/nginx/.htpasswd/{}'.format(service),
-                    user['name'], user['password']])
-        configuration = {
-            'privkey': live['privkey'],
-            'fullchain': live['fullchain'],
-            'service': service,
-            'servers': data['private_ips'],
-            'fqdns': data['fqdns'],
-            'dhparam': live['dhparam'],
-            'auth_basic': bool(data['basic_auth'])
-        }
-        if data['loadbalancing']:
-            configuration['loadbalancing'] = data['loadbalancing']
-        configure_site('{}.conf'.format(service), 'service.conf', **configuration)
+@when(
+    'ssl-termination-proxy.installed',
+    'lets-encrypt.registered',
+    'reverseproxy.available')
+@when_not(
+    'ssl-termination-proxy.running')
+def set_up(reverseproxy):
+    print('Http relation found, configuring proxy.')
+    credentials = config.get('credentials', '').split()
+    # Did we get a valid value? If not, blocked!
+    if len(credentials) not in (0, 2):
+        status_set(
+            'blocked',
+            'authentication config wrong! '
+            'I expect 2 space-separated strings. I got {}.'.format(len(credentials)))
+        return
+    # We got a valid value, signal to regenerate config.
+    try:
+        os.remove('/etc/nginx/.htpasswd')
+    except OSError:
+        pass
+    # Did we get credentials? If so, configure them.
+    if len(credentials) == 2:
+        check_call([
+            'htpasswd', '-c', '-b', '/etc/nginx/.htpasswd',
+            credentials[0], credentials[1]])
+    services = reverseproxy.services()
+    live = lets_encrypt.live()
+    template = 'encrypt.nginx.jinja2'
+    configure_site(
+        'default', template,
+        privkey=live['privkey'],
+        fullchain=live['fullchain'],
+        fqdn=config['fqdn'].rstrip(),
+        hostname=services[0]['hosts'][0]['hostname'],
+        port=services[0]['hosts'][0]['port'],
+        dhparam=live['dhparam'],
+        auth_basic=bool(config['credentials']))
     set_state('ssl-termination-proxy.running')
-    status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
+    status_set('active', 'Ready (https://{})'.format(config['fqdn'].rstrip()))
+
+
+@when(
+    'ssl-termination-proxy.running')
+@when_not(
+    'reverseproxy.available')
+def stop_nginx():
+    print('Reverseproxy relation broken')
+    remove_state('ssl-termination-proxy.running')
